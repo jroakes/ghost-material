299c670a67de639200cee324ef55c210b8a5e730
root <root@ghost.visible.company>
2015-04-06 00:56:51 -0400

    Initial Commit

---
+++ assets/js/init.js
@@ -0,0 +1,11 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+
+	$("pre").addClass("prettyprint linenums"),
+        prettyPrint()    
+
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
